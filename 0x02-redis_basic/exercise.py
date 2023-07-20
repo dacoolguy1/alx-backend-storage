8004662d7225c2e975937648956ab42c3492f209
dacoolguy1 <emmanueltemiede@gmail.com>
2023-07-20 03:15:16 -0700

    1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """method that takes a data argument and returns a string"""
 import redis
-from typing import Union
+from typing import Union, Optional
 import uuid
 
 
@@ -25,7 +25,7 @@ class Cache():
             str: _description_
         """
 
-        key = str(uuid.uuid4()) # lets convert uuid to string
+        key = str(uuid.uuid4())  # lets convert uuid to string
         # Convert data to bytes if it's not already
         if not isinstance(data, bytes):
             # convert to byte if not an instance
@@ -33,3 +33,44 @@ class Cache():
             # Store the data in Redis with the generated key
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[callable] = None) \
+            -> Union[str, bytes, int, float]:
+        """
+        Get a key
+
+        Args:
+            key (str): _description_
+            fn (Optional[callable], optional): _description_.
+            convert the data back
+            to the desired format
+
+        Returns:
+            Union[str, bytes, int, float]: _description_
+        """
+        data = self._redis.get(key)  # get this particular key
+        if data is not None:
+            if fn is not None:
+                fn(data)
+        return key
+
+    def get_str(self, key: str) -> Optional[str]:
+        """_summary_
+
+        Args:
+            self (_type_): _description_
+        """
+        return self.get(key,
+                        fn=lambda d: d.decode('utf-8')
+                        if d is not None else None)
+
+    def get_int(self, key: str) -> Optional[int]:
+        """_summary_
+
+        Args:
+            key (str): _description_
+
+        Returns:
+            Optional[int]: _description_
+        """
+        return self.get(key, fn=lambda d: int(d) if d is not None else None)
